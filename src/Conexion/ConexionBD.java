dc97573a5addcc7d22bceb2ab4af4d12300383ec
Admin <Admin@DESKTOP-MT3CF64>
2024-04-09 08:50:42 -0400

    puse algo en conexion

--- src/Conexion/ConexionBD.java
+++ src/Conexion/ConexionBD.java
@@ -1,5 +1,28 @@
 package Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
 
-}
+
+	    public static String url = " ";
+	    public static String usuario = "root";
+	    public static String clave = " ";
+	    public static String clase = " ";
+
+	    public static Connection conectar() {
+	        Connection conexion = null;
+
+	        try {
+	            Class.forName(clase);
+	            conexion = DriverManager.getConnection(url, usuario, clave);
+
+	        } catch (ClassNotFoundException | SQLException e) {
+	            System.out.println(e);
+	        }
+	        return conexion;
+	    }
+	}	
+
