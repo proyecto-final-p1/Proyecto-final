0eff37a71b6d75e0eb28d4f5bb6c63d094c6c3dc
Gabo2310 <gabrielalejandromancebo77@gmail.com>
2024-04-08 23:50:49 -0300

    Clase Persona

--- src/Persona/Persona.java
+++ src/Persona/Persona.java
@@ -1,5 +1,19 @@
 package Persona;
 
-public class Persona {
+public abstract class Persona {
+     protected String nombre;
+    protected String apellido;
+    protected int edad;
+    protected String usuario;
+    protected int contraseña;
+    protected String correo;
+
+    public Persona(String nombre, String apellido, int edad, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+    }
 
 }
+
